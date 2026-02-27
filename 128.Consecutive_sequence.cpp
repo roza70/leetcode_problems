79f7baafb4e3893e2e76371f3b2cea2107e696e2
tahsin-roza <tahsinroza10@gmail.com>
2026-02-27 19:29:39 +0600

    solved 128

---
+++ 128.Consecutive_sequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin() , nums.end());
+        int longestStreak = 0;
+        for(int num : numSet){
+            if(numSet.find(num - 1) != numSet.end()){
+                continue;
+            }
+            else{
+                int currentNum = num;
+                int currentStreak = 1;
+            
+                while(numSet.find(currentNum + 1)!= numSet.end()){
+                   currentNum++;
+                   currentStreak++;
+                }
+            longestStreak = max(longestStreak , currentStreak);
+            }
+        }
+        return longestStreak;
+
+        
+    }
+};
